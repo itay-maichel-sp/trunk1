d86ee349106e012672665526fe8d1ebc631dd667
itay <itay@wbx.test>
2015-08-04 12:35:44 +0300

    commit for confilct

--- Tools/Generators/WBXEventGenerator/src/Program.cs
+++ Tools/Generators/WBXEventGenerator/src/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System;
 using System.IO;
 using System.Threading;
@@ -20,7 +20,7 @@ namespace WBXEventGenerator {
         private const string DATA_COLLECTOR_FILE_DATA_LINE = "{0},{1}";
         private const string DATA_COLLECTOR_FILE_ERROR_LINE = "{0},ERROR: {1}";
 
-// added another comment
+// added another comment!
 
         //// Create base event for updating
         private const string BASE_EVENT =
